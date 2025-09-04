5cad8f23c801d150e951cf6170888a3cd1e9354d
FeudalCheese981 <ethanmaguire787@gmail.com>
2025-09-04 12:10:10 +0100

    Documented EBO.hpp

--- src/EBO.hpp
+++ src/EBO.hpp
@@ -4,14 +4,20 @@
 #include <glad/glad.h>
 #include <vector>
 
+// Element Buffer Object
 class EBO
 {
 	public:
+		// ID reference that opengl methods will use
 		GLuint ID;
+		// Constructor takes in indices
 		EBO(std::vector<GLuint>& indices);
 
+		// Bind the EBO to the OpenGL context
 		void Bind();
+		// Unbind the EBO from the OpenGL context
 		void Unbind();
+		// Delete the EBO
 		void Delete();
 };
 
